1ede150979d6efcb5c7a18305e6e9b2630894d95
Mike Nolta <mike@nolta.net>
2014-03-05 21:11:02 -0500

    wrap more functions

--- src/ERFA.jl
+++ src/ERFA.jl
@@ -1,8 +1,39 @@
 module ERFA
 
 export
+    eraCal2jd,
     eraD2dtf,
     eraDtf2d,
+    eraEe00a,
+    eraEe00b,
+    eraEe06a,
+    eraEect00,
+    eraEo06a,
+    eraEpb,
+    eraEpb2jd,
+    eraEpj,
+    eraEpj2jd,
+    eraEqeq94,
+    eraEra00,
+    eraFad03,
+    eraFae03,
+    eraFaf03,
+    eraFaju03,
+    eraFal03,
+    eraFalp03,
+    eraFama03,
+    eraFame03,
+    eraFane03,
+    eraFaom03,
+    eraFapa03,
+    eraFasa03,
+    eraFaur03,
+    eraFave03,
+    eraGmst82,
+    eraGst00b,
+    eraGst94,
+    eraJd2cal,
+    eraJdcalf,
     eraTaitt,
     eraTaiut1,
     eraTaiutc,
@@ -10,6 +41,7 @@ export
     eraTcgtt,
     eraTdbtcb,
     eraTdbtt,
+    eraTf2d,
     eraTttai,
     eraTttcg,
     eraTttdb,
@@ -22,6 +54,37 @@ export
 
 using BinDeps
 @BinDeps.load_dependencies
+
+function eraCal2jd(iy::Integer, imo::Integer, id::Integer)
+    r1 = [0.]
+    r2 = [0.]
+    i = ccall((:eraCal2jd,liberfa), Cint,
+              (Cint,Cint,Cint,Ptr{Float64},Ptr{Float64}),
+              iy, imo, id, r1, r2)
+    @assert i == 0
+    r1[1], r2[1]
+end
+
+function eraJd2cal(d1::Real, d2::Real)
+    iy = Int32[0]
+    imo = Int32[0]
+    id = Int32[0]
+    fd = [0.]
+    i = ccall((:eraJd2cal,liberfa), Cint,
+              (Float64,Float64,Ptr{Cint},Ptr{Cint},Ptr{Cint},Ptr{Float64}),
+              d1, d2, iy, imo, id, fd)
+    @assert i == 0
+    iy[1], imo[1], id[1], fd[1]
+end
+
+function eraJdcalf(ndp::Integer, d1::Real, d2::Real)
+    iymdf = Int32[0, 0, 0, 0]
+    i = ccall((:eraJdcalf,liberfa), Cint,
+              (Cint,Float64,Float64,Ptr{Cint}),
+              ndp, d1, d2, iymdf)
+    @assert i == 0
+    iymdf[1], iymdf[2], iymdf[3], iymdf[4]
+end
 
 function eraD2dtf(scale::ByteString, ndp::Integer, d1::Real, d2::Real)
     iy = Int32[0]
@@ -45,6 +108,47 @@ function eraDtf2d(scale::ByteString, iy::Integer, imo::Integer, id::Integer, ih:
     r1[1], r2[1]
 end
 
+function eraTf2d(s::Char, ihour::Integer, imin::Integer, sec::Real)
+    days = [0.]
+    i = ccall((:eraTf2d,liberfa), Cint,
+              (Cchar,Cint,Cint,Float64,Ptr{Float64}),
+              s, ihour, imin, sec, days)
+    @assert i == 0
+    days[1]
+end
+
+for f in (:eraFad03,
+          :eraFae03,
+          :eraFaf03,
+          :eraFaju03,
+          :eraFal03,
+          :eraFalp03,
+          :eraFama03,
+          :eraFame03,
+          :eraFane03,
+          :eraFaom03,
+          :eraFapa03,
+          :eraFasa03,
+          :eraFaur03,
+          :eraFave03)
+    @eval ($f)(d::Real) = ccall(($(Expr(:quote,f)),liberfa), Float64, (Float64,), d)
+end
+
+for f in (:eraEe00a,
+          :eraEe00b,
+          :eraEe06a,
+          :eraEect00,
+          :eraEo06a,
+          :eraEpb,
+          :eraEpj,
+          :eraEqeq94,
+          :eraEra00,
+          :eraGmst82,
+          :eraGst00b,
+          :eraGst94)
+    @eval ($f)(d1::Real, d2::Real) = ccall(($(Expr(:quote,f)),liberfa), Float64, (Float64,Float64), d1, d2)
+end
+
 for f in (:eraTaitt,
           :eraTaiutc,
           :eraTcbtdb,
@@ -62,6 +166,20 @@ for f in (:eraTaitt,
                       (Float64, Float64, Ptr{Float64}, Ptr{Float64}),
                       a, b, r1, r2)
             @assert i == 0
+            r1[1], r2[1]
+        end
+    end
+end
+
+for f in (:eraEpb2jd,
+          :eraEpj2jd)
+    @eval begin
+        function ($f)(d::Real)
+            r1 = [0.]
+            r2 = [0.]
+            ccall(($(Expr(:quote,f)),liberfa), Void,
+                  (Float64, Ptr{Float64}, Ptr{Float64}),
+                  d, r1, r2)
             r1[1], r2[1]
         end
     end
--- src/test.jl
+++ src/test.jl
@@ -6,3 +6,27 @@ u1,u2 = eraDtf2d("UTC", 2010, 7, 24, 11, 18, 7.318)
 a1,a2 = eraUtctai(u1, u2)
 t1,t2 = eraTaitt(a1, a2)
 @test eraD2dtf("tt", 3, t1, t2) == (2010,7,24,11,19,13,502)
+
+iy = 2008; imo = 2; id = 29
+ihour = 23; imin = 59; sec = 59.9
+d1,d2 = eraCal2jd(iy, imo, id)
+d = eraTf2d('+', ihour, imin, sec)
+d2 += d
+@test d1 == 2400000.5
+@test_approx_eq_eps d2 54525.999999 5e-7
+iy,imo,id,fd = eraJd2cal(d1, d2)
+@test (iy,imo,id) == (2008,2,29)
+@test_approx_eq_eps fd 0.999999 5e-7
+@test eraJdcalf(3, d1, d2) == (2008,3,1,0)
+
+d = 2457073.05631
+e = eraEpb(0., d)
+@test_approx_eq_eps e 2015.1365941021 5e-11
+d1,d2 = eraEpb2jd(e)
+d = d1 + d2
+@test_approx_eq_eps d 2457073.056310000 5e-10
+e = eraEpj(0., d)
+@test_approx_eq_eps e 2015.1349933196 5e-11
+d1,d2 = eraEpj2jd(e)
+d = d1 + d2
+@test_approx_eq_eps d 2457073.056310000 5e-10

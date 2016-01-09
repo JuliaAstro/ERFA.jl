700aea89962a3036b0711f1c3d85228f71f75e85
Kyle Barbary <kylebarbary@gmail.com>
2016-01-09 17:15:22 +0100

    check for required symbols in installed liberfa

--- deps/build.jl
+++ deps/build.jl
@@ -4,8 +4,14 @@ using BinDeps
 version = "1.2.0"
 url = "https://github.com/liberfa/erfa/releases/download/v$version/erfa-$version.tar.gz"
 
-erfa = library_dependency("liberfa")
+# This function returns true if a library satisfies our
+# requirements. BinDeps passes to this function the library path and
+# the dlopen'ed handle. In this case, we require the library have the
+# symbol `eraAb`, which is not present in liberfa 1.0.0.
+validate(l, h) = Libdl.dlsym_e(h, "eraAb") != C_NULL
+
+erfa = library_dependency("liberfa"; validate = validate)
 provides(Sources, URI(url), erfa)
 provides(BuildProcess, Autotools(libtarget="src/liberfa.la"), erfa)
 
-@BinDeps.install [:liberfa => :liberfa]
+@BinDeps.install Dict(:liberfa => :liberfa)

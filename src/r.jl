fad28aa780c7868d72eff12dd1609928a17a13b4
Helge Eichhorn <git@helgeeichhorn.de>
2020-03-27 07:11:24 +0100

    Fix r

--- src/r.jl
+++ src/r.jl
@@ -143,12 +143,12 @@ zenith distance.
 
 """
 function refco(phpa, tk, rh, wl)
-    refa = Ref(0.0)
-    refb = Ref(0.0)
+    refa = Ref{Cdouble}()
+    refb = Ref{Cdouble}()
     ccall((:eraRefco, liberfa), Cvoid,
           (Cdouble, Cdouble, Cdouble, Cdouble, Ref{Cdouble}, Ref{Cdouble}),
           phpa, tk, rh, wl, refa, refb)
-    refa[], refb[]
+    return refa[], refb[]
 end
 
 """
@@ -181,11 +181,12 @@ Express an r-matrix as an r-vector.
 
 """
 function rm2v(r)
+    @checkdims 3 3 r
     w = zeros(3)
     ccall((:eraRm2v, liberfa), Cvoid,
           (Ref{Cdouble}, Ref{Cdouble}),
-          r, w)
-    w
+          permutedims(r), w)
+    return w
 end
 
 """
@@ -215,11 +216,12 @@ Form the r-matrix corresponding to a given r-vector.
 
 """
 function rv2m(w)
+    @checkdims 3 w
     r = zeros((3, 3))
     ccall((:eraRv2m, liberfa), Cvoid,
           (Ref{Cdouble}, Ref{Cdouble}),
           w, r)
-    r
+    return permutedims(r)
 end
 
 """
@@ -247,12 +249,13 @@ Multiply two r-matrices.
 
 """
 function rxr(a, b)
+    @checkdims 3 3 a b
     atb = zeros((3, 3))
     ccall((:eraRxr, liberfa),
           Cvoid,
           (Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}),
-          a, b, atb)
-    atb
+          permutedims(a), permutedims(b), atb)
+    return permutedims(atb)
 end
 
 """
@@ -352,10 +355,12 @@ for name in ("rx",
     fc = "era" * uppercasefirst(name)
     @eval begin
         function ($f)(a, r)
+            @checkdims 3 3 r
+            _r = permutedims(r)
             ccall(($fc, liberfa), Cvoid,
                   (Cdouble, Ref{Cdouble}),
-                  a, r)
-            r
+                  a, _r)
+            return permutedims(_r)
         end
     end
 end
@@ -383,12 +388,14 @@ Multiply a pv-vector by an r-matrix.
 - `eraRxp`: product of r-matrix and p-vector
 
 """
-function rxpv(r, p)
-    rp = zeros((2, 3))
+function rxpv(r, pv)
+    @checkdims 3 3 r
+    _pv = array_to_cmatrix(pv; n=3)
+    rpv = zeros(Cdouble, 3, 2)
     ccall((:eraRxpv, liberfa), Cvoid,
             (Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}),
-            r, p, rp)
-    rp
+            permutedims(r), _pv, rpv)
+    return cmatrix_to_array(rpv)
 end
 
 """
@@ -423,3 +430,4 @@ function rxp(r, p)
             permutedims(r), p, rp)
     return rp
 end
+
--- test/r.jl
+++ test/r.jl
@@ -11,44 +11,47 @@ end
 
 # ERFA.rm2v
 @testset "rm2v" begin
-    w = ERFA.rm2v([[0.0,-0.8,-0.6];
-                   [0.8,-0.36,0.48];
-                   [0.6,0.48,-0.64]])
+    w = ERFA.rm2v([0.0 -0.8 -0.6;
+                   0.8 -0.36 0.48;
+                   0.6 0.48 -0.64])
     @test isapprox(w[1], 0.0, atol = 1e-12)
     @test isapprox(w[2], 1.413716694115406957, atol = 1e-12)
     @test isapprox(w[3], -1.884955592153875943, atol = 1e-12)
+    @test_throws ArgumentError ERFA.rm2v([0.0 -0.8 -0.6])
 end
 
 # ERFA.rv2m
 @testset "rv2m" begin
     r = ERFA.rv2m([0.0, 1.41371669, -1.88495559])
-    @test isapprox(r[1], -0.7071067782221119905, atol = 1e-14)
-    @test isapprox(r[2], -0.5656854276809129651, atol = 1e-14)
-    @test isapprox(r[3], -0.4242640700104211225, atol = 1e-14)
-    @test isapprox(r[4], 0.5656854276809129651, atol = 1e-14)
-    @test isapprox(r[5], -0.0925483394532274246, atol = 1e-14)
-    @test isapprox(r[6], -0.8194112531408833269, atol = 1e-14)
-    @test isapprox(r[7], 0.4242640700104211225, atol = 1e-14)
-    @test isapprox(r[8], -0.8194112531408833269, atol = 1e-14)
-    @test isapprox(r[9], 0.3854415612311154341, atol = 1e-14)
+    @test isapprox(r[1,1], -0.7071067782221119905, atol = 1e-14)
+    @test isapprox(r[1,2], -0.5656854276809129651, atol = 1e-14)
+    @test isapprox(r[1,3], -0.4242640700104211225, atol = 1e-14)
+    @test isapprox(r[2,1], 0.5656854276809129651, atol = 1e-14)
+    @test isapprox(r[2,2], -0.0925483394532274246, atol = 1e-14)
+    @test isapprox(r[2,3], -0.8194112531408833269, atol = 1e-14)
+    @test isapprox(r[3,1], 0.4242640700104211225, atol = 1e-14)
+    @test isapprox(r[3,2], -0.8194112531408833269, atol = 1e-14)
+    @test isapprox(r[3,3], 0.3854415612311154341, atol = 1e-14)
+    @test_throws ArgumentError ERFA.rv2m([1.41371669, -1.88495559])
 end
 
 # ERFA.rx
 @testset "rx" begin
     phi = 0.3456789
-    r = [[2.0,3.0,2.0];
-         [3.0,2.0,3.0];
-         [3.0,4.0,5.0]]
+    r = [2.0 3.0 2.0;
+         3.0 2.0 3.0;
+         3.0 4.0 5.0]
     r = ERFA.rx(phi, r)
-    @test isapprox(r[1], 2.0, atol = 0.0)
-    @test isapprox(r[2], 3.0, atol = 0.0)
-    @test isapprox(r[3], 2.0, atol = 0.0)
-    @test isapprox(r[4], 3.839043388235612460, atol = 1e-12)
-    @test isapprox(r[5], 3.237033249594111899, atol = 1e-12)
-    @test isapprox(r[6], 4.516714379005982719, atol = 1e-12)
-    @test isapprox(r[7], 1.806030415924501684, atol = 1e-12)
-    @test isapprox(r[8], 3.085711545336372503, atol = 1e-12)
-    @test isapprox(r[9], 3.687721683977873065, atol = 1e-12)
+    @test isapprox(r[1,1], 2.0, atol = 0.0)
+    @test isapprox(r[1,2], 3.0, atol = 0.0)
+    @test isapprox(r[1,3], 2.0, atol = 0.0)
+    @test isapprox(r[2,1], 3.839043388235612460, atol = 1e-12)
+    @test isapprox(r[2,2], 3.237033249594111899, atol = 1e-12)
+    @test isapprox(r[2,3], 4.516714379005982719, atol = 1e-12)
+    @test isapprox(r[3,1], 1.806030415924501684, atol = 1e-12)
+    @test isapprox(r[3,2], 3.085711545336372503, atol = 1e-12)
+    @test isapprox(r[3,3], 3.687721683977873065, atol = 1e-12)
+    @test_throws ArgumentError ERFA.rx(phi, r[1:2,:])
 end
 
 # ERFA.rxp
@@ -74,73 +77,81 @@ end
 
 # ERFA.rxpv
 @testset "rxpv" begin
-    r = [[2.0,3.0,2.0];
-         [3.0,2.0,3.0];
-         [3.0,4.0,5.0]]
-    pv = [[0.2,1.5,0.1];
+    r = [2.0 3.0 2.0;
+         3.0 2.0 3.0;
+         3.0 4.0 5.0]
+    pv = [[0.2,1.5,0.1],
           [1.5,0.2,0.1]]
     rpv = ERFA.rxpv(r, pv)
-    @test isapprox(rpv[1], 5.1, atol = 1e-12)
-    @test isapprox(rpv[4], 3.8, atol = 1e-12)
-    @test isapprox(rpv[2], 3.9, atol = 1e-12)
-    @test isapprox(rpv[5], 5.2, atol = 1e-12)
-    @test isapprox(rpv[3], 7.1, atol = 1e-12)
-    @test isapprox(rpv[6], 5.8, atol = 1e-12)
+    @test isapprox(rpv[1][1], 5.1, atol = 1e-12)
+    @test isapprox(rpv[1][2], 3.9, atol = 1e-12)
+    @test isapprox(rpv[1][3], 7.1, atol = 1e-12)
+    @test isapprox(rpv[2][1], 3.8, atol = 1e-12)
+    @test isapprox(rpv[2][2], 5.2, atol = 1e-12)
+    @test isapprox(rpv[2][3], 5.8, atol = 1e-12)
+    @test_throws ArgumentError ERFA.rxpv(r[1:2,:], pv)
+    pve = [[1.5,0.1],
+          [1.5,0.2,0.1]]
+    @test_throws ArgumentError ERFA.rxpv(r, pve)
 end
 
 # ERFA.rxr
 @testset "rxr" begin
-    a = [[2.0,3.0,2.0];
-         [3.0,2.0,3.0];
-         [3.0,4.0,5.0]]
-    b = [[1.0,2.0,2.0];
-         [4.0,1.0,1.0];
-         [3.0,0.0,1.0]]
+    a = [2.0 3.0 2.0;
+         3.0 2.0 3.0;
+         3.0 4.0 5.0]
+    b = [1.0 2.0 2.0;
+         4.0 1.0 1.0;
+         3.0 0.0 1.0]
     atb = ERFA.rxr(a, b)
-    @test isapprox(atb[1], 20.0, atol = 1e-12)
-    @test isapprox(atb[2], 7.0, atol = 1e-12)
-    @test isapprox(atb[3], 9.0, atol = 1e-12)
-    @test isapprox(atb[4], 20.0, atol = 1e-12)
-    @test isapprox(atb[5], 8.0, atol = 1e-12)
-    @test isapprox(atb[6], 11.0, atol = 1e-12)
-    @test isapprox(atb[7], 34.0, atol = 1e-12)
-    @test isapprox(atb[8], 10.0, atol = 1e-12)
-    @test isapprox(atb[9], 15.0, atol = 1e-12)
+    @test isapprox(atb[1,1], 20.0, atol = 1e-12)
+    @test isapprox(atb[1,2], 7.0, atol = 1e-12)
+    @test isapprox(atb[1,3], 9.0, atol = 1e-12)
+    @test isapprox(atb[2,1], 20.0, atol = 1e-12)
+    @test isapprox(atb[2,2], 8.0, atol = 1e-12)
+    @test isapprox(atb[2,3], 11.0, atol = 1e-12)
+    @test isapprox(atb[3,1], 34.0, atol = 1e-12)
+    @test isapprox(atb[3,2], 10.0, atol = 1e-12)
+    @test isapprox(atb[3,3], 15.0, atol = 1e-12)
+    @test_throws ArgumentError ERFA.rxr(a[1:2,:], b)
+    @test_throws ArgumentError ERFA.rxr(a, b[1:2,:])
 end
 
 # ERFA.ry
 @testset "ry" begin
     theta = 0.3456789
-    r = [[2.0,3.0,2.0];
-         [3.0,2.0,3.0];
-         [3.0,4.0,5.0]]
+    r = [2.0 3.0 2.0;
+         3.0 2.0 3.0;
+         3.0 4.0 5.0]
     r = ERFA.ry(theta, r)
-    @test isapprox(r[1], 0.8651847818978159930, atol = 1e-12)
-    @test isapprox(r[2], 1.467194920539316554, atol = 1e-12)
-    @test isapprox(r[3], 0.1875137911274457342, atol = 1e-12)
-    @test isapprox(r[4], 3, atol = 1e-12)
-    @test isapprox(r[5], 2, atol = 1e-12)
-    @test isapprox(r[6], 3, atol = 1e-12)
-    @test isapprox(r[7], 3.500207892850427330, atol = 1e-12)
-    @test isapprox(r[8], 4.779889022262298150, atol = 1e-12)
-    @test isapprox(r[9], 5.381899160903798712, atol = 1e-12)
+    @test isapprox(r[1,1], 0.8651847818978159930, atol = 1e-12)
+    @test isapprox(r[1,2], 1.467194920539316554, atol = 1e-12)
+    @test isapprox(r[1,3], 0.1875137911274457342, atol = 1e-12)
+    @test isapprox(r[2,1], 3, atol = 1e-12)
+    @test isapprox(r[2,2], 2, atol = 1e-12)
+    @test isapprox(r[2,3], 3, atol = 1e-12)
+    @test isapprox(r[3,1], 3.500207892850427330, atol = 1e-12)
+    @test isapprox(r[3,2], 4.779889022262298150, atol = 1e-12)
+    @test isapprox(r[3,3], 5.381899160903798712, atol = 1e-12)
+    @test_throws ArgumentError ERFA.ry(theta, r[1:2,:])
 end
 
 # ERFA.rz
 @testset "rz" begin
     psi = 0.3456789
-    r = [[2.0,3.0,2.0];
-         [3.0,2.0,3.0];
-         [3.0,4.0,5.0]]
+    r = [2.0 3.0 2.0;
+         3.0 2.0 3.0;
+         3.0 4.0 5.0]
     r = ERFA.rz(psi, r)
-    @test isapprox(r[1], 2.898197754208926769, atol = 1e-12)
-    @test isapprox(r[2], 3.500207892850427330, atol = 1e-12)
-    @test isapprox(r[3], 2.898197754208926769, atol = 1e-12)
-    @test isapprox(r[4], 2.144865911309686813, atol = 1e-12)
-    @test isapprox(r[5], 0.865184781897815993, atol = 1e-12)
-    @test isapprox(r[6], 2.144865911309686813, atol = 1e-12)
-    @test isapprox(r[7], 3.0, atol = 1e-12)
-    @test isapprox(r[8], 4.0, atol = 1e-12)
-    @test isapprox(r[9], 5.0, atol = 1e-12)
+    @test isapprox(r[1,1], 2.898197754208926769, atol = 1e-12)
+    @test isapprox(r[1,2], 3.500207892850427330, atol = 1e-12)
+    @test isapprox(r[1,3], 2.898197754208926769, atol = 1e-12)
+    @test isapprox(r[2,1], 2.144865911309686813, atol = 1e-12)
+    @test isapprox(r[2,2], 0.865184781897815993, atol = 1e-12)
+    @test isapprox(r[2,3], 2.144865911309686813, atol = 1e-12)
+    @test isapprox(r[3,1], 3.0, atol = 1e-12)
+    @test isapprox(r[3,2], 4.0, atol = 1e-12)
+    @test isapprox(r[3,3], 5.0, atol = 1e-12)
+    @test_throws ArgumentError ERFA.rz(psi, r[1:2,:])
 end
 

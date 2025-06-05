a605515d3918c24726ce870bfe6a4ce1069cc292
Ian Weaver <weaveric@gmail.com>
2025-06-04 22:20:27 -0700

    typo

--- docs/make.jl
+++ docs/make.jl
@@ -7,12 +7,12 @@ makedocs(
         prettyurls = get(ENV, "CI", nothing) == "true",
         size_threshold_warn = 400 * 1024, # 400 KiB
         size_threshold = 800 * 1024, # 800 KiB
+        canonical = "https://juliaastro.org/ERFA/stable/",
     ),
     sitename = "ERFA.jl",
     authors = "The JuliaAstro Contributors",
     pages = pages,
     doctest = true,
-    canonical = "https://juliaastro.org/ERFA/stable"
 )
 
 deploydocs(
